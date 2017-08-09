dad8b7e6f9faad9473100f52ed5b57fefbcf89c6
sanchittanwar7 <tanwarsanchit9762@gmail.com>
2017-08-09 20:30:43 +0530

    products added

--- index.js
+++ index.js
@@ -1,86 +1,49 @@
-$(document).ready(function() {
-  
-  $("#menu-section, #nav-bar-item-menu").click(function(e){
-      e.preventDefault();
-      $("html, body").animate({scrollTop: 700},"slow");
-  });
-  
-  $(document).scroll(function() {
-    if ($(this).scrollTop() > 650) {
-      $('#nav-bar-overlay').fadeIn(120);
+$(function(){
+  $('.b-slogan__button-play').magnificPopup({
+    //delegate: '.b-slogan__url', // child items selector, by clicking on it popup will open
+    type: 'iframe',
+    tLoading: 'Loading video #%curr%...',
+    modal:false,
+    markup: '<div class="mfp-iframe-scaler">'+
+            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+          '</div>', 
+    callbacks:{
+      open: function(){
+        $('.mfp-close').remove();//удаление старой кнопки
+        $('<button title="" type="button" class="mfp-close"></button>').appendTo('.mfp-container');
+        //var content = $(".mfp-iframe");
+      }
     }
-    
-    if ($(this).scrollTop() < 650) {
-      $('#nav-bar-overlay').fadeOut(120);
-    }
   });
-  
-  $("#drinks-section, #nav-bar-item-drinks").click(function(e){
-      e.preventDefault();
-      $("html, body").animate({scrollTop: 2300},"slow");
-  });
-  
-  $("#location-section").click(function(e){
-      e.preventDefault();
-      $("html, body").animate({scrollTop: 3361}, 1250);
-  });
-  
-  $("#nav-bar-item-top").click(function(e){
-      e.preventDefault();
-      $("html, body").animate({scrollTop: 0}, 1250);
-  });
-  
-  $("#contact-section, #nav-bar-item-contact").click(function(e){
-      e.preventDefault();
-      $("html, body").animate({scrollTop: 3361}, 1250);
-  });
-
-  $('#reservation-button').click(function() {        
-      $("#dialog-box").fadeIn(150);
-      $("#dialog-overlay").fadeIn(150);
-  });
-  
-  $("#close-dialog-menu").click(function() {        
-      $("#dialog-box").fadeOut(150);
-      $("#dialog-overlay").fadeOut(150);
-  });
-  
-  $("#dialog-overlay").click(function() {        
-      $("#dialog-box").fadeOut(150);
-      $("#dialog-overlay").fadeOut(150);
-  });
-  
-  $("#dinner-menu-bar-text").click(function() {
-    $(this).css("color", "#ff8b38");
-    $("#when-available-dinner").css("color", "#ff8b38");
-
-    $("#lunch-menu-bar-text").css("color", "#c9c9c9");
-    $("#when-available-lunch").css("color", "#c9c9c9");
-    
-    $("#happy-hour-bar-text").css("color", "#c9c9c9");
-    $("#when-available-happy").css("color", "#c9c9c9");
-  });
-  
-  $("#happy-hour-bar-text").click(function() {
-    $(this).css("color", "#ff8b38");
-    $("#when-available-happy").css("color", "#ff8b38");
-
-    $("#lunch-menu-bar-text").css("color", "#c9c9c9");
-    $("#when-available-lunch").css("color", "#c9c9c9");
-    
-    $("#dinner-menu-bar-text").css("color", "#c9c9c9");
-    $("#when-available-dinner").css("color", "#c9c9c9");
-  });
-  
-  $("#lunch-menu-bar-text").click(function() {
-    $(this).css("color", "#ff8b38");
-    $("#when-available-lunch").css("color", "#ff8b38");
-
-    $("#happy-hour-bar-text").css("color", "#c9c9c9");
-    $("#when-available-happy").css("color", "#c9c9c9");
-    
-    $("#dinner-menu-bar-text").css("color", "#c9c9c9");
-    $("#when-available-dinner").css("color", "#c9c9c9");
-  });
-  
 });
+
+
+
+function goTo(value){
+  // console.log(value.href.split('#')[1]);
+  tagName = value.href.split('#')[1];
+  var tag = document.getElementsByTagName('div');
+  // console.log(tag);
+  // console.log(tag[0].baseURI/*split('#')[1]*/);
+  for(var i=0;i<tag.length;i++){
+    if(tag[i].id == tagName){
+      tagName = tag[i];
+      break;
+    }
+  }
+  pos = tagName.getBoundingClientRect();
+  // console.log(pos.top);
+  var currentY = window.scrollY;
+  var finalY = pos.top;
+  var delta = (currentY - finalY)/100;
+  number = setInterval(function(){
+    currentY = currentY - delta;
+    window.scrollTo(0,currentY);
+    mod = currentY-finalY;
+    if(mod<0){
+      mod = mod*(-1);
+    }
+    if(mod <= 1)
+      clearInterval(number);
+  },5);
+}
